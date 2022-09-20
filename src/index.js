c22eef38af6a8b5b7253995009e52dff4cf1072b
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-09-20 19:24:23 +0300

    Created a new jsx file to confirm if my program is working

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { ReactDOM } from "react";
+import App from "./App"
+import "./index.css"
+
+//it takes two arguments the HTML code and the element hence it displays the HTML code inside the specified element.
+ReactDom.render(<App/>, document.querySelector("#root"))
