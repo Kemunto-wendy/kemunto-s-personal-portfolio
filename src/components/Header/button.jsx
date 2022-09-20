b2012a75627828f5186be3aa6fbfecb9454c0730
Kemunto-wendy <joy.kemunto@student.moringaschool.com>
2022-09-20 21:59:37 +0300

    additional features on my header file

---
+++ src/components/Header/button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./header.css"
+
+const Button = ( ) => {
+    return(
+        <div className="button">
+            <a href=""> Download CV </a>
+            <a href=""> View Resume </a>
+        </div>
+    )
+}
+
+export default Button
--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -3,9 +3,14 @@ import "./header.css"
 
 const Header = ( ) => {
     return(
-        <div>
-            APP
+     <header>
+        <div className="container">
+            <h5>Hey There!!!!</h5>
+            <h4>Get to know me</h4>
+            <h1>I'm JOY <span>KEMUNTO</span></h1>
+            <h5>A Junior <span>Front  End</span> developer</h5>
         </div>
+     </header>
     )
 }
 
